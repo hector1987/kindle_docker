237d5d7003fe1163b8a9ec591141a42b5ba36353
hector1987 <46617440+hector1987@users.noreply.github.com>
2024-08-26 23:52:40 +0900

    Prevent updater

--- start_kindle.sh
+++ start_kindle.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+shopt -s expand_aliases
+
+alias docker_kindle='docker run -it --rm -u 2000:2000 -e DISPLAY=${DISPLAY} -v "/tmp/.X11-unix/:/tmp/.X11-unix:rw" -v winehome:/home/wineuser --ipc host kindle_img'
+
 xhost +si:localuser:#2000
-docker run --rm -u 2000:2000 -e DISPLAY=${DISPLAY} -v "/tmp/.X11-unix/:/tmp/.X11-unix:rw" -v winehome:/home/wineuser --ipc host kindle_img /bin/bash -c "wine '/home/wineuser/.wine/dosdevices/c:/Program Files (x86)/Amazon/Kindle/Kindle.exe'"
+docker_kindle /bin/bash -c "rm '/home/wineuser/.wine/dosdevices/c:/users/wineuser/AppData/Local/Amazon/Kindle/updates/KindleForPC-installer.exe'"
+docker_kindle /bin/bash -c "wine '/home/wineuser/.wine/dosdevices/c:/Program Files (x86)/Amazon/Kindle/Kindle.exe'"
+#docker_kindle /bin/bash -c "sleep 10"
 xhost -si:localuser:#2000
